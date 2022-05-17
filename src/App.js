3e674eb899d1e9708a17f5a1c2a8ba8638a20837
Leonardo Puchetta <rissottona@gmail.com>
2022-05-17 10:23:01 -0300

    cambios2

--- src/App.js
+++ src/App.js
@@ -6,10 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p> app react para prueba en github 
+        <p> app react para prueba en github 2.0
         </p>
-        <p>Acabo de hacer estos cambios</p>
-        <p>Acabo de hacer estos cambios</p><p>Acabo de hacer estos cambios</p>
+       
         <a
           className="App-link"
           href="https://reactjs.org"
