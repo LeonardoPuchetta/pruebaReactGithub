0eaeb631ad79bd3f1ccc83d05aa021468abfa863
Leonardo Puchetta <rissottona@gmail.com>
2022-05-17 10:09:56 -0300

    cambios y deploy

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p> app react para prueba en github 
         </p>
+        <p>Acabo de hacer estos cambios</p>
         <a
           className="App-link"
           href="https://reactjs.org"
